bf99d54e3d5d710fe810f5bfc51c864c5d456577
proyect_icaro <josuevalenzuela87@gmail.com>
2023-08-19 14:54:17 -0400

    feature: folders create for app

---
+++ frontend_tienda/tienda-app/src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+  
+
+  return (
+    <>
+    <h1 className=" flex  justify-center text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    </>
+  )
+}
+
+export default App
